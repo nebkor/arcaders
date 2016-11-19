b3a1d0d7a072dc473b7b35084326c571bd58b96e
Joe Ardent <code@ardent.nebcorp.com>
2016-11-18 23:16:54 -0800

    first stab at 60fps

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ fn main() {
     // Initialize SDL2
     let sdl_context = sdl2::init().unwrap();
     let video = sdl_context.video().unwrap();
+    let mut timer = sdl_context.timer().unwrap();
 
     // Create the window
     let window = video.window("ArcadeRS Shooter", 800, 600)
@@ -28,7 +29,32 @@ fn main() {
 
     let mut current_view: Box<View> = Box::new(::views::DefaultView);
 
+    // frame timing
+    let interval = 1_000.0 / 60.0;
+    let mut before = timer.ticks();
+    let mut last_second = timer.ticks();
+    let mut fps = 0u16;
+
     loop {
+
+        let now = timer.ticks();
+        let dt = now - before;
+        let elapsed = dt as f64 / 1_000.0;
+
+        if (elapsed * 60.0) < 1.0 {
+            timer.delay(interval as u32 - dt);
+            continue;
+        }
+
+        before = now;
+        fps += 1;
+
+        if now - last_second > 999 {
+            println!("FPS: {}", fps);
+            last_second = now;
+            fps = 0;
+        }
+
         context.events.pump();
 
         match current_view.render(&mut context, 0.01) {
